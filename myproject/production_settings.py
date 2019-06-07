43f14c43397d41523f5f72d57725020cf5d8e50e
Hardcase <hardcase@inbox.ru>
2019-06-07 17:13:33 +0300

    pre-release 14

--- myproject/production_settings.py
+++ myproject/production_settings.py
@@ -294,7 +294,7 @@ CACHES = {
         #'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', },
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
